589df04f243da271eae9e028a411868c1c77c16d
luciaherrrador <luciaherradorcarrasco@gmail.com>
2019-12-02 16:23:17 +0100

    add html structure and fetch function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
+//console.log('>> Ready :)');
+const input = document.querySelector("#search-input");
+const button = document.querySelector("#search-button");
+const result = document.querySelector("#series-container");
+const urlBase = 'http://api.tvmaze.com/search/shows?q=';
+let list = [];
 
-console.log('>> Ready :)');
+const getDatafromApi = event => {
+    event.preventDefault();
+    const inputValue = input.value.toLowerCase();
+    fetch(urlBase + inputValue)
+        .then(response => response.json())
+        .then(data => {
+            list = data;
+            paintSeries(data);
+        });
+};
+
+button.addEventListener('click', getDatafromApi);
